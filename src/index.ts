9e818c086563e86f2e21225987504dae11a1dbf4
Yann SEGET <dev@leafgard.fr>
2021-08-14 01:04:58 +0200

    feat: add clockwise and counter-clockwise matrix rotations

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,42 @@
-export function rotate (): void {
+export enum RotationDirection {
+  CLOCKWISE = 'CLOCKWISE',
+  COUNTER_CLOCKWISE = 'COUNTER_CLOCKWISE'
+}
 
+/**
+ * Rotate 2-dimensional matrix
+ * @param {T[][]} matrix Matrix to rotate
+ * @param {RotationDirection} rotationDirection Rotation direction
+ * @returns {T[][]} Rotated matrix
+ */
+export function rotate2d <T extends {}> (
+  matrix: T[][],
+  rotationDirection: RotationDirection = RotationDirection.CLOCKWISE
+): T[][] {
+  const columns: T[][] = []
+  for (let x = 0; x < matrix.length; x++) {
+    columns.push(
+      getColumn(matrix, x, rotationDirection)
+    )
+  }
+  return rotationDirection === RotationDirection.CLOCKWISE ? columns : columns.reverse()
+}
+
+/**
+ * Rotate matrix column by index
+ * @param {T[][]} matrix Matrix in which we rotate a column
+ * @param {number} columnIndex Column index
+ * @param {RotationDirection} rotationDirection Rotation direction
+ * @returns {T[]} Rotated matrix column
+ */
+function getColumn <T extends {}> (
+  matrix: T[][],
+  columnIndex: number,
+  rotationDirection: RotationDirection
+): T[] {
+  const column: T[] = []
+  for (let i = 0; i < matrix.length; i++) {
+    column.push(matrix[i][columnIndex])
+  }
+  return rotationDirection === RotationDirection.CLOCKWISE ? column.reverse() : column
 }
--- tests/rotate.test.ts
+++ tests/rotate.test.ts
@@ -1,7 +1,33 @@
 /* eslint-env jest */
 
-import { rotate } from '../src'
+import { rotate2d, RotationDirection } from '../src'
 
-test('90deg 2-dimensional matrix rotation', () => {
-  expect(rotate()).toBeUndefined()
+describe('2-dimensional matrix rotation', () => {
+  test('clockwise', () => {
+    const initial = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+    const rotated = [
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ]
+    expect(rotate2d(initial)).toEqual(rotated)
+  })
+
+  test('counter-clockwise', () => {
+    const initial = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+    const rotated = [
+      [3, 6, 9],
+      [2, 5, 8],
+      [1, 4, 7]
+    ]
+    expect(rotate2d(initial, RotationDirection.COUNTER_CLOCKWISE)).toEqual(rotated)
+  })
 })
